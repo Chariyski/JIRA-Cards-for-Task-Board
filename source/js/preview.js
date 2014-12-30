389de723b6107661a5e52e79d460cf3fd56eff4a
Krasimir Chariyski <krasimir.chariyski@sap.com>
2014-12-31 00:29:25 +0200

    save options - integrated

--- source/js/preview.js
+++ source/js/preview.js
@@ -18,15 +18,36 @@
 (function () {
     'use strict';
 
+    var cardTemplate = document.getElementById('template-type'),
+        fromAssignee = document.getElementById('assignees'),
+        tactBold = document.getElementById('bold-tact'),
+        priorityBold = document.getElementById('bold-priority'),
+        keyBold = document.getElementById('bold-key'),
+        subtaskBold = document.getElementById('bold-subtask'),
+        summaryBold = document.getElementById('bold-summary'),
+        assigneeBold = document.getElementById('bold-assignee'),
+        tactShow = document.getElementById('show-tact'),
+        priorityShow = document.getElementById('show-priority'),
+        keyShow = document.getElementById('show-key'),
+        subtaskShow = document.getElementById('show-subtask'),
+        summaryShow = document.getElementById('show-summary'),
+        assigneeShow = document.getElementById('show-assignee'),
+        fontSize = document.getElementById('font-size'),
+        headingFontSize = document.getElementById('heading-size');
+
     var page,
         card,
         model,
-        store;
+        options;
 
+    // Add Page events listeners
     window.addEventListener('load', function () {
         var form = document.getElementById('option-form'),
             printPreviewButton = document.getElementById('print-preview'),
             saveButton = document.getElementById('save');
+
+        // If there are any saved option, apples them.
+        options.apply();
 
         // Listener for data, send from getInfoFromJIRA.js
         chrome.runtime.onMessage.addListener(
@@ -37,8 +58,8 @@
                     return;
                 }
 
-                // Check if the received data is the needed one
-                if (request.issues === undefined || request.issues.length < 1) {
+                // Check if there is data in the request
+                if (request.issues === undefined) {
                     page.message.showError();
                     page.message.hideLoading();
 
@@ -101,27 +122,26 @@
 
         // Save form options
         saveButton.addEventListener('click', function () {
-            store.saveOptions();
-            store.getOptions();
+            options.save();
         }, false);
 
     }, false);
 
     /**
      * Object that processes the received data
-     * @type {{issues: undefined, isReceivedDataValid: Function, getIssues: Function, setIssues: Function}}
+     * @type {{_issues: undefined, getIssues: Function, setIssues: Function}}
      */
     model = {
 
         // All issues received from JIRA server
-        issues: undefined,
+        _issues: undefined,
 
         /**
          * Get issues
          * @returns {Object}
          */
         getIssues: function () {
-            return this.issues;
+            return this._issues;
         },
 
         /**
@@ -130,14 +150,14 @@
          * @returns {model}
          */
         setIssues: function (data) {
-            this.issues = data;
+            this._issues = data;
             return this;
         }
     };
 
     /**
      * Manipulates page elements
-     * @type {{insertAssignees: Function, insertCSSRule: Function, message: {hideError: Function, hideInfo: Function, hideLoading: Function, showError: Function, showInfo: Function, showLoading: Function}}}
+     * @type {{insertAssignees: Function, message: {hideError: Function, hideInfo: Function, hideLoading: Function, showError: Function, showInfo: Function, showLoading: Function}}}
      */
     page = {
 
@@ -146,7 +166,6 @@
          */
         insertAssignees: function () {
             var issues = model.getIssues(),
-                assigneesSelect = document.getElementById('assignees'),
                 uniqueAssignees = [];
 
             for (var i = 0; i < issues.length; i++) {
@@ -158,20 +177,12 @@
             for (var j = 0; j < uniqueAssignees.length; j++) {
                 var newOptionTag = document.createElement('option');
                 newOptionTag.innerText = uniqueAssignees[j];
-                assigneesSelect.appendChild(newOptionTag);
+                fromAssignee.appendChild(newOptionTag);
             }
         },
 
-        /**
-         * Insert CSS rule in the last style sheet on the page
-         * @param cssRule {string} CSS rule
-         */
-        insertCSSRule: function (cssRule) {
-            var sheet = document.styleSheets[document.styleSheets.length - 1];
-            sheet.insertRule(cssRule, sheet.cssRules.length);
-        },
-
         // Message handling
+        // TODO opravi stilovete na popupa
         message: {
             hideError: function () {
                 document.getElementById('message-error').style.display = 'none';
@@ -195,10 +206,52 @@
     };
 
     /**
-     * Create/Delete Cards into/from the page
-     * @type {{deleteCards: Function, changeDisplay: Function, changeFontWeight: Function, changeFontSize: Function, changeHeadingFontSize: Function, createCards: Function, getTemplate: Function, setTemplate: Function, showFromAssignees: Function}}
+     * Manipulates Cards
+     * @type {{changeDisplay: Function, changeFontWeight: Function, changeFontSize: Function, changeHeadingFontSize: Function, deleteCards: Function, createCards: Function, _insertCSSRule: Function, showFromAssignees: Function}}
      */
     card = {
+
+        /**
+         * Change element display CSS property
+         * @param CSSClassName{String} The element class name, which need to be changed
+         * @param isVisible {boolean} true for displaying element, false for not displaying the element
+         */
+        changeDisplay: function (CSSClassName, isVisible) {
+            if (isVisible) {
+                this._insertCSSRule('.' + CSSClassName + '{display:flex;}');
+            } else {
+                this._insertCSSRule('.' + CSSClassName + '{display:none;}');
+            }
+        },
+
+        /**
+         * Change font weight
+         * @param CSSClassName {String} The element class name, which need to be changed
+         * @param isBold {boolean} If true the element will be bold
+         */
+        changeFontWeight: function (CSSClassName, isBold) {
+            if (isBold) {
+                this._insertCSSRule('.' + CSSClassName + '{font-weight:900;}');
+            } else {
+                this._insertCSSRule('.' + CSSClassName + '{font-weight:100;}');
+            }
+        },
+
+        /**
+         * Change font size on all card elements
+         * @param fontSize {String} font size value
+         */
+        changeFontSize: function (fontSize) {
+            this._insertCSSRule('.card{ font-size:' + fontSize + '; }');
+        },
+
+        /**
+         * Change font size on the summary
+         * @param fontSize {String} font size value
+         */
+        changeHeadingFontSize: function (fontSize) {
+            this._insertCSSRule('.summary{ font-size:' + fontSize + '; }');
+        },
 
         /**
          * Delete all created cards
@@ -211,53 +264,11 @@
         },
 
         /**
-         * Change element display CSS property
-         * @param elementClassName {String} The element class name, which need to be changed
-         * @param isShown {boolean} true for displaying element, false for not displaying the element
-         */
-        changeDisplay: function (elementClassName, isShown) {
-            if (isShown === true) {
-                page.insertCSSRule('.card .' + elementClassName + '{ display: flex; }');
-            } else {
-                page.insertCSSRule('.card .' + elementClassName + '{  display: none; }');
-            }
-        },
-
-        /**
-         * Change font weight
-         * @param elementClassName {String} The element class name, which need to be changed
-         * @param isBold {boolean} If true the element will be bold
-         */
-        changeFontWeight: function (elementClassName, isBold) {
-            if (isBold === true) {
-                page.insertCSSRule('.card .' + elementClassName + '{ font-weight: 900; }');
-            } else {
-                page.insertCSSRule('.card .' + elementClassName + '{ font-weight: 100; }');
-            }
-        },
-
-        /**
-         * Change font size on all card elements
-         * @param fontSize {String} font size value
-         */
-        changeFontSize: function (fontSize) {
-            page.insertCSSRule('.card{ font-size:' + fontSize + 'px; }');
-        },
-
-        /**
-         * Change font size on the summary
-         * @param fontSize {String} font size value
-         */
-        changeHeadingFontSize: function (fontSize) {
-            page.insertCSSRule('.summary{ font-size:' + fontSize + 'px; }');
-        },
-
-        /**
          * Create cards for task board
          */
         createCards: function () {
             var issues = model.getIssues(),
-                template = document.getElementById(this.getTemplate()),
+                template = document.getElementById(cardTemplate.value),
                 cardsWrapper = document.getElementById('cards-container'),
                 fragment = new DocumentFragment();
 
@@ -310,22 +321,12 @@
         },
 
         /**
-         * Get the ID of the chosen card template
-         * @returns {string}
+         * Insert CSS rule in the last style sheet on the page
+         * @param cssRule {string} CSS rule
          */
-        getTemplate: function () {
-            return document.getElementById('template-type').value;
-        },
-
-        /**
-         * Get the ID of the chosen card template
-         * @param templateID {String}
-         * @returns {card}
-         */
-        setTemplate: function (templateID) {
-            document.getElementById('template-type').value = templateID;
-
-            return this;
+        _insertCSSRule: function (cssRule) {
+            var sheet = document.styleSheets[document.styleSheets.length - 1];
+            sheet.insertRule(cssRule, sheet.cssRules.length);
         },
 
         /**
@@ -333,64 +334,192 @@
          * @param assignee {String}
          */
         showFromAssignees: function (assignee) {
-            page.insertCSSRule('.card[data-assignee]{ display:flex; }');
+            this._insertCSSRule('.card[data-assignee]{ display:flex; }');
 
             if (assignee === 'assigned') {
-                page.insertCSSRule('.card[data-assignee="unassigned"]{ display:none; }');
-            } else if (event.target.value !== 'all') {
-                page.insertCSSRule('.card:not([data-assignee="' + assignee + '"]){ display:none; }');
+                this._insertCSSRule('.card[data-assignee="unassigned"]{ display:none; }');
+            } else if (assignee !== 'all') {
+                this._insertCSSRule('.card:not([data-assignee="' + assignee + '"]){ display:none; }');
             }
         }
     };
 
-    store = {
-        applyOptions: function () {
+    /**
+     * Responsible for options
+     * @type {{apply: Function, save: Function}}
+     */
+    options = {
 
-        },
-        getOptions: function () {
-            chrome.storage.sync.get('JCTB', function (obj) {
-                console.log(obj);
+        /**
+         * Apples all saved options
+         */
+        apply: function () {
+            chrome.storage.sync.get('cards', function (object) {
+                var options;
+
+                if (object.cards === undefined) {
+                    return;
+                }
+
+                options = object.cards;
+
+                // Set template
+                if (options.template !== cardTemplate.value) {
+                    cardTemplate.value = options.template;
+                }
+
+                // Set fromAssignees
+                if (options.fromAssignees !== fromAssignee.value) {
+                    fromAssignee.value = options.fromAssignees;
+
+                    card.showFromAssignees(fromAssignee.value);
+                }
+
+                // Set bold style
+                if (options.fields.tact.bold !== tactBold.checked) {
+                    tactBold.checked = options.fields.tact.bold;
+
+                    card.changeFontWeight(tactShow.getAttribute('data-class'), tactShow.checked);
+                }
+
+                // Set bold style
+                if (options.fields.priority.bold !== priorityBold.checked) {
+                    priorityBold.checked = options.fields.priority.bold;
+
+                    card.changeFontWeight(priorityShow.getAttribute('data-class'), priorityShow.checked);
+                }
+
+                // Set bold style
+                if (options.fields.key.bold !== keyBold.checked) {
+                    keyBold.checked = options.fields.key.bold;
+
+                    card.changeFontWeight(subtaskShow.getAttribute('data-class'), subtaskShow.checked);
+                }
+
+                // Set bold style
+                if (options.fields.subtask.bold !== subtaskBold.checked) {
+                    subtaskBold.checked = options.fields.subtask.bold;
+
+                    card.changeFontWeight(keyShow.getAttribute('data-class'), keyShow.checked);
+                }
+
+                // Set bold style
+                if (options.fields.summary.bold !== summaryBold.checked) {
+                    summaryBold.checked = options.fields.summary.bold;
+
+                    card.changeFontWeight(summaryShow.getAttribute('data-class'), summaryShow.checked);
+                }
+
+                // Set bold style
+                if (options.fields.assignee.bold !== assigneeBold.checked) {
+                    assigneeBold.checked = options.fields.assignee.bold;
+
+                    card.changeFontWeight(assigneeShow.getAttribute('data-class'), assigneeShow.checked);
+                }
+
+                // Set display style
+                if (options.fields.tact.show !== tactShow.checked) {
+                    tactShow.checked = options.fields.tact.show;
+
+                    card.changeDisplay(tactShow.getAttribute('data-class'), tactShow.checked);
+                }
+
+                // Set display style
+                if (options.fields.priority.show !== priorityShow.checked) {
+                    priorityShow.checked = options.fields.priority.show;
+
+                    card.changeDisplay(priorityShow.getAttribute('data-class'), priorityShow.checked);
+                }
+
+                // Set display style
+                if (options.fields.subtask.show !== subtaskShow.checked) {
+                    subtaskShow.checked = options.fields.subtask.show;
+
+                    card.changeDisplay(subtaskShow.getAttribute('data-class'), subtaskShow.checked);
+                }
+
+                // Set display style
+                if (options.fields.key.show !== keyShow.checked) {
+                    keyShow.checked = options.fields.key.show;
+
+                    card.changeDisplay(keyShow.getAttribute('data-class'), keyShow.checked);
+                }
+
+                // Set display style
+                if (options.fields.summary.show !== summaryShow.checked) {
+                    summaryShow.checked = options.fields.summary.show;
+
+                    card.changeDisplay(summaryShow.getAttribute('data-class'), summaryShow.checked);
+                }
+
+                // Set display style
+                if (options.fields.assignee.show !== assigneeShow.checked) {
+                    assigneeShow.checked = options.fields.assignee.show;
+
+                    card.changeDisplay(assigneeShow.getAttribute('data-class'), assigneeShow.checked);
+                }
+
+                // Set font style
+                if (options.fields.fontSize !== fontSize.value) {
+                    fontSize.value = options.fields.fontSize;
+
+                    card.changeFontSize(fontSize.value);
+                }
+
+                // Set font style
+                if (options.fields.headingFontSize !== headingFontSize.value) {
+                    headingFontSize.value = options.fields.headingFontSize;
+
+                    card.changeFontSize(fontSize.value);
+                }
+
             });
         },
-        saveOptions: function () {
+
+        /**
+         * Save options form state
+         */
+        save: function () {
             var options = {
-                'JCTB': {
-                    template: undefined,
-                    assignee: undefined,
+                'cards': {
+                    template: cardTemplate.value,
+                    fromAssignees: fromAssignee.value,
                     fields: {
                         tact: {
-                            bold: undefined,
-                            show: undefined
+                            bold: tactBold.checked,
+                            show: tactShow.checked
                         },
                         priority: {
-                            bold: undefined,
-                            show: undefined
+                            bold: priorityBold.checked,
+                            show: priorityShow.checked
                         },
                         key: {
-                            bold: undefined,
-                            show: document.getElementById('show-key').checked
+                            bold: keyBold.checked,
+                            show: keyShow.checked
                         },
                         subtask: {
-                            bold: undefined,
-                            show: undefined
+                            bold: subtaskBold.checked,
+                            show: subtaskShow.checked
                         },
                         summary: {
-                            bold: undefined,
-                            show: undefined
+                            bold: summaryBold.checked,
+                            show: summaryShow.checked
                         },
                         assignee: {
-                            bold: undefined,
-                            show: undefined
+                            bold: assigneeBold.checked,
+                            show: assigneeShow.checked
                         },
-                        fontSize: undefined,
-                        headingFontSize: undefined
+                        fontSize: fontSize.value,
+                        headingFontSize: headingFontSize.value
                     }
                 }
             };
 
             chrome.storage.sync.set(options, function () {
+                // TODO
                 console.log('Saved');
             });
         }
-    }
+    };
+
 }());
