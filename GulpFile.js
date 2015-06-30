57be677ce34ed344f0763ffd35d084c6597521ee
Krasimir Chariyski <kr.chariyski@gmail.com>
2015-06-30 21:30:47 +0300

    Change Grunt to Gulp build

---
+++ GulpFile.js
@@ -0,0 +1,70 @@
+var gulp = require('gulp');
+
+var clean = require('gulp-clean'),
+    copy = require('gulp-copy'),
+    concat = require('gulp-concat'),
+    sourcemaps = require('gulp-sourcemaps'),
+    crisper = require('gulp-crisper'),
+    vulcanize = require('gulp-vulcanize'),
+    watch = require('gulp-watch'),
+    plumber = require('gulp-plumber'),
+    runSequence = require('gulp-run-sequence');
+
+
+// Delete dist folder
+gulp.task('clean', function () {
+    return gulp.src('dist', {read: false})
+        .pipe(plumber())
+        .pipe(clean());
+});
+
+// Concatenate all JS files that are needed for distribution
+gulp.task('concat', function () {
+    return gulp.src([
+        'bower_components/webcomponentsjs/webcomponents-lite.min.js'
+    ])
+        .pipe(plumber())
+        .pipe(sourcemaps.init())
+        .pipe(concat('bundle.js'))
+        .pipe(sourcemaps.write())
+        .pipe(gulp.dest('app/scripts'));
+});
+
+// Copy all files that are needed for distribution
+gulp.task('copy', function () {
+    return gulp.src(
+        ['manifest.json',
+            'images/*.*',
+            'scripts/bundle.js',
+            'popup.html'
+        ], {cwd: 'app'})
+        .pipe(plumber())
+        .pipe(copy('dist'));
+});
+
+// Preprocessed HTML files
+gulp.task('vulcanize', function () {
+    return gulp.src('app/html/elements.html')
+        .pipe(plumber())
+        .pipe(vulcanize({
+            abspath: '',
+            inlineScripts: true,
+            inlineCss: true,
+            stripExcludes: false,
+            excludes: ['//fonts.googleapis.com/*']
+        }))
+        .pipe(crisper())
+        .pipe(gulp.dest('dist/html'));
+});
+
+// Watch for changes
+gulp.task('watch', function () {
+    gulp.watch('app/manifest.json', ['copy']);
+    gulp.watch('app/**/*.html', ['copy', 'vulcanize']);
+});
+
+gulp.task('build', function (callback) {
+    runSequence('clean', ['concat', 'copy', 'vulcanize'], callback);
+});
+gulp.task('default', ['build']);
+gulp.task('serve', ['build', 'watch']);
