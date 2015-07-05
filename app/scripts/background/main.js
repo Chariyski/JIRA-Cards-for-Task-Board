8efeed54e39f37f32fd293a2a715a6f79ea031e7
Krasimir Chariyski <kr.chariyski@gmail.com>
2015-07-05 12:07:05 +0300

    Added sample visualization for issues

--- app/scripts/background/main.js
+++ app/scripts/background/main.js
@@ -1,5 +1,5 @@
 chrome.browserAction.onClicked.addListener(function (tab) {
     chrome.tabs.create({
-        url: "chrome-extension://mbkgkdflhiaamohkeicabcpglogajbgd/"
+        url: "chrome-extension://mbkgkdflhiaamohkeicabcpglogajbgd/index.html"
     }); // TODO change when it is public
 });
--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,37 +1,6 @@
-/*
- Copyright (c) 2015 The Polymer Project Authors. All rights reserved.
- This code may only be used under the BSD style license found at http://polymer.github.io/LICENSE.txt
- The complete set of authors may be found at http://polymer.github.io/AUTHORS.txt
- The complete set of contributors may be found at http://polymer.github.io/CONTRIBUTORS.txt
- Code distributed by Google as part of the polymer project is also
- subject to an additional IP rights grant found at http://polymer.github.io/PATENTS.txt
- */
-
 (function (document) {
     'use strict';
 
-    function getFromJIRA(url) {
-        return new Promise(function (resolve, reject) {
-            var request = new XMLHttpRequest();
-
-            request.onreadystatechange = function () {
-                if (request.readyState !== 4) {
-                    return;
-                }
-
-                if (request.status === 200) {
-                    // If successful, resolve the promise by passing back the request response
-                    resolve(JSON.parse(request.responseText));
-                } else {
-                    // If it fails, reject the promise with a error message
-                    reject(Error('Error status: ' + request.statusText));
-                }
-            };
-
-            request.open('GET', window.location.protocol + '//' + window.location.host + url, true);
-            request.send();
-        });
-    }
 
     // Grab a reference to our auto-binding template
     // and give it some initial binding values
