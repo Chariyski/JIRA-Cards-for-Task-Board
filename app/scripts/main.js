b460620f65bf137432906f424a58b099d1b741c0
Krasimir Chariyski <krasimir.chariyski@gmail.com>
2015-07-09 21:34:15 +0300

    Added options storage logic

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,15 +1,10 @@
 (function (document) {
     'use strict';
 
-
     // Grab a reference to our auto-binding template
     // and give it some initial binding values
     // Learn more about auto-binding templates at http://goo.gl/Dx1u2g
     var app = document.querySelector('#app');
-
-    app.displayInstalledToast = function () {
-        document.querySelector('#caching-complete').show();
-    };
 
     // Listen for template bound event to know when bindings
     // have resolved and content has been stamped to the page
@@ -20,6 +15,7 @@
     // See https://github.com/Polymer/polymer/issues/1381
     window.addEventListener('WebComponentsReady', function () {
         // imports are loaded and elements have been registered
+        app.loadUserOptions();
     });
 
     // Close drawer after menu item is selected if drawerPanel is narrow
@@ -48,6 +44,34 @@
 
     app.onJIRAIssuesResponse = function () {
         document.querySelector('#ajax-spinner').active = false;
+    };
+
+    ////////////////////////////////////////////////////////////
+    // Extension storage
+    ////////////////////////////////////////////////////////////
+
+    app.loadUserOptions = function () {
+        chrome.storage.sync.get('ajax', function (object) {
+            app.URLAddress = object.ajax.url;
+            app.project = object.ajax.project;
+            app.projectVersion = object.ajax.version;
+
+            document.querySelector('#options-loaded').show();
+        });
+    };
+
+    app.saveUserOptions = function () {
+        var options = {
+            ajax: {
+                url: app.URLAddress || 'https://sapjira.wdf.sap.corp/',
+                project: app.project || 'BGSOFUIRODOPI',
+                version: app.projectVersion || '101511'
+            }
+        };
+
+        chrome.storage.sync.set(options, function () {
+            document.querySelector('#options-saved').show();
+        });
     };
 
 })(document);
