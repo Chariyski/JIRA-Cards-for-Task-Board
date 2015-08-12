7332d671c93f66326592051bc9ee672c883b70c2
Krasimir Chariyski <kr.chariyski@gmail.com>
2015-08-12 10:06:17 +0300

    fixed routing selection

--- app/scripts/background/main.js
+++ app/scripts/background/main.js
@@ -1,5 +1,5 @@
 chrome.browserAction.onClicked.addListener(function (tab) {
     chrome.tabs.create({
-        url: "chrome-extension://" + chrome.app.getDetails().id + "/index.html"
+        url: "chrome-extension://" + chrome.app.getDetails().id + "/index.html#!/home"
     });
 });
