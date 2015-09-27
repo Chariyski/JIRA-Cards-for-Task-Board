e134c52bd5d18ed8210bd3e3e7dea7b6486d0837
Alexandra Chariyska <alex.chariyska@gmail.com>
2015-09-27 14:35:48 +0300

    Browser action text for new version added

--- app/scripts/background/main.js
+++ app/scripts/background/main.js
@@ -1,5 +1,13 @@
 chrome.browserAction.onClicked.addListener(function (tab) {
     chrome.tabs.create({
-        url: "chrome-extension://" + chrome.app.getDetails().id + "/index.html#!/home"
+        url: 'chrome-extension://' + chrome.app.getDetails().id + '/index.html#!/home'
     });
+    chrome.browserAction.setBadgeText({text: ''});
+});
+
+chrome.runtime.onInstalled.addListener(function (details) {
+    var isVersionUpdated = chrome.runtime.getManifest().version.split('.')[2] === '0';
+    if (details.reason === 'update' && isVersionUpdated) {
+        chrome.browserAction.setBadgeText({text: 'New'});
+    }
 });
